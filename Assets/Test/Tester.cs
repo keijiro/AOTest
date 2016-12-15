29ad0d396750b8ee7eaf943661bd7c4757d5d011
Keijiro Takahashi <keijiro@gmail.com>
2016-12-15 17:47:00 +0900

    Added tester script/shader.

---
+++ Assets/Test/Tester.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace AOTest
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    public class Tester : MonoBehaviour
+    {
+        [SerializeField] Texture2D _referenceImage;
+        [SerializeField, Range(0, 1)] float _mix;
+        [SerializeField] bool _showDifference;
+
+        [SerializeField, HideInInspector] Shader _shader;
+
+        Material _material;
+
+        void OnEnable()
+        {
+            _material = new Material(Shader.Find("Hidden/AOTest/Tester"));
+            _material.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        void OnDestroy()
+        {
+            if (Application.isPlaying)
+                Destroy(_material);
+            else
+                DestroyImmediate(_material);
+        }
+
+        [ImageEffectOpaque]
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            _material.SetTexture("_RefTex", _referenceImage);
+            _material.SetFloat("_Mix", _mix);
+
+            if (_showDifference)
+                _material.EnableKeyword("_DIFF_MODE");
+            else
+                _material.DisableKeyword("_DIFF_MODE");
+
+            Graphics.Blit(source, destination, _material, 0);
+        }
+    }
+}
---
+++ Assets/Test/Tester.shader
@@ -0,0 +1,42 @@
+﻿Shader "Hidden/AOTest/Tester"
+{
+    Properties
+    {
+        _MainTex("", 2D) = ""{}
+        _RefTex("", 2D) = ""{}
+    }
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+    sampler2D _RefTex;
+
+    float _Mix;
+
+    half4 frag(v2f_img input) : SV_Target
+    {
+        float c_i = saturate(tex2D(_MainTex, input.uv).r);
+        float c_r = saturate(tex2D(_RefTex, input.uv).r);
+        #if defined(_DIFF_MODE)
+        return saturate(c_r - c_i) + float4(saturate(c_i - c_r), 0, 0, 0);
+        #else
+        return lerp(c_i, c_r, _Mix);
+        #endif
+    }
+
+    ENDCG
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+        Pass
+        {
+            CGPROGRAM
+            #pragma multi_compile _ _DIFF_MODE
+            #pragma vertex vert_img
+            #pragma fragment frag
+            #pragma target 3.0
+            ENDCG
+        }
+    }
+}
